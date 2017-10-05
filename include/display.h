73c2a832f79bfa6494a6c03deb33da6a5e62df2c
shakram02 <ahmedhamdyau@gmail.com>
2017-10-05 02:44:12 +0200

    load GLEW

--- include/display.h
+++ include/display.h
@@ -1,23 +1,23 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
-#include <string>
+#include <GL/glew.h>
 #include <SDL2/SDL.h>
+#include <iostream>
+#include <string>
 
-class Display
-{
+class Display {
 public:
-    Display(int width, int height, const std::string& title);
-    virtual ~Display();
-    void update();
-    bool isClosed();
+  Display(int width, int height, const std::string &title);
+  virtual ~Display();
+  void update();
+  bool isClosed();
+
 protected:
-
 private:
-
-    SDL_Window* sdlWindow;
-    SDL_GLContext sdlContext;
-    bool closed;
+  SDL_Window *sdlWindow;
+  SDL_GLContext sdlContext;
+  bool closed;
 };
 
 #endif // DISPLAY_H
--- src/display.cpp
+++ src/display.cpp
@@ -1,47 +1,50 @@
 #include "display.h"
 #include <iostream>
-Display::Display(int width, int height, const std::string& title)
-{
-    SDL_Init(SDL_INIT_EVERYTHING);
+Display::Display(int width, int height, const std::string &title) {
+  SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_GL_SetAttribute(SDL_GL_RED_SIZE,8);
-    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE,8);
-    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,8);
-    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE,8);
-    SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE,32);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
+  SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
-    this->sdlWindow =
-        SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,width,height,SDL_WINDOW_OPENGL);
+  this->sdlWindow = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED,
+                                     SDL_WINDOWPOS_CENTERED, width, height,
+                                     SDL_WINDOW_OPENGL);
+  this->sdlContext = SDL_GL_CreateContext(this->sdlWindow);
+  this->closed = false;
 
-    this->sdlContext = SDL_GL_CreateContext(this->sdlWindow);
+  GLenum res = glewInit();
 
-    this->closed = false;
+  if (res != GLEW_OK) {
+    std::cerr << "Glew failed to initialize!" << std::endl;
+  }
+
+  glEnable(GL_DEPTH_TEST);
+
+  glEnable(GL_CULL_FACE);
+  glCullFace(GL_BACK);
 }
 
-Display::~Display()
-{
-    // Destroy objects in the reverse order they're created
-    SDL_GL_DeleteContext(this->sdlContext);
-    SDL_DestroyWindow(this->sdlWindow);
-    SDL_Quit();
+Display::~Display() {
+  // Destroy objects in the reverse order they're created
+  SDL_GL_DeleteContext(this->sdlContext);
+  SDL_DestroyWindow(this->sdlWindow);
+  SDL_Quit();
 }
 
-void Display::update()
-{
-    SDL_GL_SwapWindow(this->sdlWindow);
+void Display::update() {
+  SDL_GL_SwapWindow(this->sdlWindow);
 
-    SDL_Event e;
+  SDL_Event e;
 
-    while(SDL_PollEvent(&e))
-    {
-        if( e.type == SDL_QUIT){
-            this->closed = true;
-        }
+  while (SDL_PollEvent(&e)) {
+    if (e.type == SDL_QUIT) {
+      this->closed = true;
     }
+  }
 }
 
-bool Display::isClosed(){
-    return this->closed;
-}
-
+bool Display::isClosed() { return this->closed; }
