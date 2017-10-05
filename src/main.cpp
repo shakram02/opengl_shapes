6400b54e904312b0813691bac36287ea59df0926
shakram02 <ahmedhamdyau@gmail.com>
2017-10-05 15:56:12 +0200

    add event handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,46 @@
 #include "display.hpp"
 #include "shader.hpp"
-#include <GL/glew.h>
-#include <iostream>
+#include "mesh.hpp"
 
 #define SHADER_FOLDER_NAME "./script/"
 
 using namespace std;
 
 int main() {
-  Display d(800, 600, "Cool");
+    Display window(800, 600, "Test");
 
-  Shader s(std::string(SHADER_FOLDER_NAME) + std::string("basic_shader"));
+    Vertex vertices[] = {
+            // Values are from -1 to 1
+            // -1 left/bottom, 1 is right/top
+            Vertex(glm::vec3(-0.5, -0.5, 0)),
+            Vertex(glm::vec3(0, 0.5, 0)),
+            Vertex(glm::vec3(0.5, -0.5, 0))
+    };
 
-  s.bind();
+    Shader shader(std::string(SHADER_FOLDER_NAME) + std::string("basic_shader"));
+    Mesh mesh(vertices, (sizeof(vertices) / sizeof(Vertex)));
 
-  while (!d.isClosed()) {
-    glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    bool isRunning = true;
+    while (isRunning) {
 
-    d.update();
-  }
+        SDL_Event e;
 
-  return 0;
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                isRunning = false;
+            }
+
+            if (e.type == SDL_KEYDOWN && e.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
+                isRunning = false;
+            }
+        }
+
+        window.clear(0.15f, 0.2f, 0.15f, 1.0f);
+
+        shader.bind();
+        mesh.draw();
+
+        window.swapBuffers();
+    }
+    return 0;
 }
